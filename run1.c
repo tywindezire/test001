4712ce5d779d2c22e3c5745dd49faf6c8b000578
tywindezire <tywinfordezire@gmail.com>
2021-04-05 09:56:14 +0530

    Changed to Java

--- run1.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main()
-{
-  printf("First Test on Heroku");
-  return 0;
-}
-
---
+++ run1.java
@@ -0,0 +1,8 @@
+class run1
+{
+  public static void main(String args[])
+  {
+    System.out.println("Hello!");
+  }
+}
+
